22ff17025f2da6330a0c3704a6317e30c18441eb
cyvision13 <cyvision13@outlook.com>
2021-02-02 04:08:44 -0500

    Connect to the database (MongoDB)

--- server/server.js
+++ server/server.js
@@ -2,7 +2,20 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose.connect(
+    "mongodb+srv://root:VhECxzpgo8CXGdN4@amazon-clone.eao6f.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to the database.");
+        }
+    }
+);
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
