00cfe218e942983b2f590a9f22a2c98d88aa43aa
cyvision13 <cyvision13@outlook.com>
2021-02-02 03:43:23 -0500

    Create a user User Schema

--- server/server.js
+++ server/server.js
@@ -8,17 +8,16 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.get('/', (req, res) => {
-    res.json("helllo amazon clone")
-})
-app.post('/', (req, res) => {
+app.get("/", (req, res) => {
+    res.json("helllo amazon clone");
+});
+app.post("/", (req, res) => {
     console.log(req.body.name);
-})
+});
 app.listen(3000, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log('Listening on PORT,3000');
+        console.log("Listening on PORT,3000");
     }
-})
+});
