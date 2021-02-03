44136b7b90761c8fad6fa9cd993c78f2cc038a71
cyvision13 <cyvision13@outlook.com>
2021-02-03 12:19:31 -0500

    Owner and Category Schema

---
+++ server/models/owner.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const OwnerSchema = new Schema({
+    name: String,
+    about: String,
+    photo: String
+})
+module.exports = mongoose.model("Owner", OwnerSchema)
