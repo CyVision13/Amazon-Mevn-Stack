1786bd04c5fe6bc4c660bc9888e28d0d7a732293
cyvision13 <cyvision13@outlook.com>
2021-02-05 05:01:45 -0500

    Delete fkn amazon s3 service

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,26 +4,28 @@ const Product = require("./../models/product");
 
 const upload = require('../middlewares/upload-photo')
     // POST request - create a new product
-router.post("/products", upload.single('photo'), async(req, res) => {
-    try {
-        let product = new Product();
-        product.title = req.body.title;
-        product.description = req.body.description;
-        product.photo = req.body.photo;
-        product.stockQuantity = req.body.stockQuantity;
-        await product.save();
+router.post("/products",
+    // upload.single('photo'),
+    async(req, res) => {
+        try {
+            let product = new Product();
+            product.title = req.body.title;
+            product.description = req.body.description;
+            product.photo = req.body.photo;
+            product.stockQuantity = req.body.stockQuantity;
+            await product.save();
 
-        res.json({
-            status: true,
-            message: "Successfully saved",
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message,
-        });
-    }
-});
+            res.json({
+                status: true,
+                message: "Successfully saved",
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message,
+            });
+        }
+    });
 
 // Get request - get all products
 
