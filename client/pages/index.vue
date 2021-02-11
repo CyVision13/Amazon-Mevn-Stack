1be3b562f9d459c82f5fbd1cc38be3632a22efb0
cyvision13 <cyvision13@outlook.com>
2021-02-10 23:31:07 -0500

    Configure CSS Files

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
     <div>
       <Logo />
       <h1 class="title">
@@ -32,42 +33,5 @@ export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
