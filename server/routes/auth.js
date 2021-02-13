ccf30b64f5878f26df7f8d37c82f357b2b7e3c70
cyvision13 <cyvision13@outlook.com>
2021-02-13 13:05:55 -0500

    Profile Page Part 2 Update Feature

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -35,14 +35,37 @@ router.post("/auth/signup", async(req, res) => {
 /* Profile Route */
 
 router.get('/auth/user', verifyToken, async(req, res) => {
-    try {
-        let foundUser = await User.findOne({ _id: req.decoded._id })
-        if (foundUser) {
-            res.json({
-                success: true,
-                user: foundUser
+        try {
+            let foundUser = await User.findOne({ _id: req.decoded._id })
+            if (foundUser) {
+                res.json({
+                    success: true,
+                    user: foundUser
+                })
+            }
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message
             })
         }
+    })
+    /*Update a profile */
+router.put("/auth/user", verifyToken, async(req, res) => {
+    try {
+        let foundUser = await User.findOne({ _id: req.decoded._id })
+
+        if (foundUser) {
+            if (req.body.name) foundUser.name = req.body.name
+            if (req.body.email) foundUser.email = req.body.email
+            if (req.body.password) foundUser.password = req.body.password
+        }
+        await foundUser.save()
+
+        res.json({
+            success: true,
+            message: 'Succesfully Updated'
+        })
     } catch (err) {
         res.status(500).json({
             success: false,
