b18359008ac4ffbae41ddca990189449d1acee44
cyvision13 <cyvision13@outlook.com>
2021-03-22 07:21:37 -0400

    Fix Err

--- client/store/index.js
+++ client/store/index.js
@@ -9,7 +9,7 @@ export const actions = {
         if (!cartProduct) {
             commit("pushProductToCart", product)
         } else {
-            commit("incrementProductQty", product)
+            commit("incrementProductQty", cartProduct)
         }
 
         commit("incrementCartLength")
